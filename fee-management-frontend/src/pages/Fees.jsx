59424dfb4191c4e97b9f16e86b863f2d622df24d
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-09-17 01:19:54 +0530

    update fee record feature complete

--- fee-management-frontend/src/pages/Fees.jsx
+++ fee-management-frontend/src/pages/Fees.jsx
@@ -4,7 +4,13 @@ import axios from "axios";
 const Fees = ()=>{
     const [feeRecord,setFeeRecord] = useState(null);
     const [id,setId] = useState("")
-    
+    const [updatedFeeRecord,setUpdatedFeeRecord] = useState(null);
+    const [form, setForm] = useState({
+        year:"",
+        amount:"",
+        paid_amount:"",
+        status:""
+    })
 
     const getFees = async()=>{
         const token = localStorage.getItem("token")
@@ -17,6 +23,7 @@ const Fees = ()=>{
             }
             )
             setFeeRecord(result.data);
+            setId("")
         } catch (error) {
             console.error(error);
             console.log(error);
@@ -26,21 +33,32 @@ const Fees = ()=>{
 
     const updateFeeRecord = async()=>{
         const token = localStorage.getItem("token");
-        const result = await axios.put(`http://localhost:5000/api/fees/${id}`,
-            {
-                year,
-                amount,
-                paid_status,
-                status
-            },
-            {
-                headers:{
-                    Authorization:`Bearer ${token}`
+        try {
+                const result = await axios.put(`http://localhost:5000/api/fees/${id}`,
+                
+                  form
+                ,
+                {
+                    headers:{
+                        Authorization:`Bearer ${token}`
+                    }
                 }
-            }
-        )
+            );
+            setUpdatedFeeRecord(result.data);
+            alert("student's fee records are updated mah man!")
+        } catch (error) {
+            console.error(error);
+            alert("Error updating fee records")
+        }
+        
+    };
 
-
+    const handleChange =(e)=>{
+        const {name,value} = e.target;
+        setForm((prev)=>({
+            ...prev,
+            [name]:["year","paid_amount","amount"].includes(name)? Number(value) : value
+        }))
     }
 
     return(
@@ -54,18 +72,46 @@ const Fees = ()=>{
             <div>
                 {feeRecord? 
                 <div>
-                    <p>Name:{feeRecord.name}</p>
-                    <p>ID:{feeRecord.student_id}</p>
-                    <p>Class:{feeRecord.class}</p>
-                    <p>contact:{feeRecord.contact}</p>
-                    <p>Year:{feeRecord.year}</p>
-                    <p>Amount:{feeRecord.amount}</p>
-                    <p>Paid Amount:{feeRecord.paid_amount}</p>
-                    <p>Status:{feeRecord.status}</p>
+                    <ul>
+                        <li>
+                            <p>Name:{feeRecord.name}</p>
+                            <p>ID:{feeRecord.student_id}</p>
+                            <p>Class:{feeRecord.class}</p>
+                            <p>contact:{feeRecord.contact}</p>
+                            <p>Year:{feeRecord.year}</p>
+                            <p>Amount:{feeRecord.amount}</p>
+                            <p>Paid Amount:{feeRecord.paid_amount}</p>
+                            <p>Status:{feeRecord.status}</p>
+                        </li>
+                    </ul>
                  </div>
                 :
                 
                 <p>Enter id to see fee record</p>}
+            </div>
+
+            <div>
+                <input type="text" placeholder="Enter the id" value={id} onChange={(e)=>{setId(e.target.value)}} />
+                <input type="text" placeholder="enter the year" name="year" value={form.year} onChange={handleChange} />
+                <input type="text" placeholder="enter the amount" name="amount" value={form.amount} onChange={handleChange}    />
+                <input type="text" placeholder="enter the paid amount" name="paid_amount" value={form.paid_amount} onChange={handleChange} /> 
+                <input type="text" placeholder="enter the status" name="status" value={form.status} onChange={handleChange} />
+                <button onClick={updateFeeRecord}>Update Record </button>
+            </div>
+            <div>
+                {updatedFeeRecord? 
+                <div>
+                    <ul>
+                        
+                        <li>{updatedFeeRecord.student_id}</li>
+                        <li>{updatedFeeRecord.year}</li>
+                        <li>{updatedFeeRecord.amount}</li>
+                        <li>{updatedFeeRecord.paid_amount}</li>
+                        <li>{updatedFeeRecord.status}</li>
+                    </ul>
+                </div>
+                :
+                <p>Enter the details above to update fee records</p>}
             </div>
 
         </div>
