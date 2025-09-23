07eaf014846a3ea5cae58550061bf04ebe72e4be
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-09-23 23:57:11 +0530

    added context

--- fee-management-frontend/src/context/AuthContext.js
+++ fee-management-frontend/src/context/AuthContext.js
@@ -1,1 +1,13 @@
 import {createContext, useContext, useState, useEffect} from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({children})=>{
+    const [isAdmin,setIsAdmin] = useState(false);
+
+    useEffect(()=>{
+        const token = localStorage.getItem("token");
+        setIsAdmin(!!token);// it is simple suppose when ther eis no token in LS then LS return null and !null will be true and one more ! will make it false 
+    },[]);
+
+}
